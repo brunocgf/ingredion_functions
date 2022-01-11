aeeb350aed7ec910f55bcf37dca458531b09da89
Bruno <brunocgf@gmail.com>
2022-01-11 13:30:43 -0700

    phase 2 funcion clean

---
+++ f_clean/__init__.py
@@ -0,0 +1,56 @@
+import io, logging, re
+import numpy as np
+import pandas as pd
+
+import azure.functions as func
+
+# container_input = "raw-data"
+# container_output = "clean-data"
+
+def format_file(df):
+    df_clean = df.copy()
+
+    #   Change column name
+    df_clean.columns.values[2] = 'company_code_id'
+    #   Reset index
+    df_clean = df_clean.reset_index(drop = True)
+    #   Fill all NANs with 0
+    df_clean = df_clean.fillna(0)
+    #   Remove the "Result" line
+    df_clean = df_clean[df_clean['Company Code'] != 'Result']
+    #   Split fiscal year and order data frame
+    dates = pd.DataFrame(data = df_clean['Fiscal year/period'].astype(str).str.split(pat = '.', expand = True).astype(int).values, columns = ['month', 'year'])
+    #  This is to have the correct value for years, have trouble with years that finish in 0 because it has been read as number.
+    dates.year[dates.year < 2016] = dates.year[dates.year < 2016]*10
+    # month
+    df_clean.insert(loc = 1, column = 'month', value = dates.iloc[:, 0])
+    # year
+    df_clean.insert(loc = 2, column = 'year', value = dates.iloc[:, 1])
+    # Sort
+    df_clean=df_clean.sort_values(['year', 'month'], ascending=[True, True])
+    df_clean=df_clean.drop(columns=['year', 'month'])
+
+    return df_clean
+
+def main(myblob: func.InputStream, outputblob: func.Out[func.InputStream]):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {myblob.name}\n"
+                 f"Blob Size: {myblob.length} bytes")
+
+    #name = myblob.name
+    #country_test = re.compile("US|CAN|MEX")
+    #country = country_test.search(name).group()
+    #name = f"{country}/"+name[:name.find(country)-1]+".parquet"
+
+    stream = myblob.read()
+    #file = io.BytesIO()
+    #stream.readinto(file)
+    df = pd.read_excel(stream, sheet_name="Data", skiprows=range(0, 15), usecols = "G:BC" )
+    df = format_file(df)
+    #print("Uploading: ", name)
+    
+    parquet_file = io.BytesIO()
+    df.to_parquet(parquet_file, engine='pyarrow')
+    parquet_file.seek(0)
+    outputblob.set(parquet_file)
+    #outputblob.upload_blob(data=parquet_file)
