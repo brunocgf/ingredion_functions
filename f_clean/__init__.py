1944ad78b862755c749de72b2608416b2f13a8bf
Bruno <brunocgf@gmail.com>
2022-01-19 12:29:02 -0700

    db

--- f_clean/__init__.py
+++ f_clean/__init__.py
@@ -1,6 +1,7 @@
-import io, logging, re
+import io, logging, re, urllib, yaml
 import numpy as np
 import pandas as pd
+import sqlalchemy
 
 import azure.functions as func
 
@@ -28,7 +29,8 @@ def format_file(df):
     df_clean.insert(loc = 2, column = 'year', value = dates.iloc[:, 1])
     # Sort
     df_clean=df_clean.sort_values(['year', 'month'], ascending=[True, True])
-    df_clean=df_clean.drop(columns=['year', 'month'])
+    #df_clean=df_clean.drop(columns=['year', 'month'])
+    df_clean['Material'] = df_clean['Material'].astype(str)
 
     return df_clean
 
@@ -45,9 +47,25 @@ def main(myblob: func.InputStream, outputblob: func.Out[func.InputStream]):
     stream = myblob.read()
     #file = io.BytesIO()
     #stream.readinto(file)
-    df = pd.read_excel(stream, sheet_name="Data", skiprows=range(0, 15), usecols = "G:BC" )
+    df = pd.read_excel(stream, 
+        sheet_name="Data", 
+        skiprows=range(0, 15), 
+        usecols = "G:BC")
+    #    dtype={'Material': str})
     df = format_file(df)
-    #print("Uploading: ", name)
+
+    with open("credentialsdb.yml") as stream:
+        cred = yaml.safe_load(stream)
+
+    params = urllib.parse.quote_plus(
+        f"DRIVER={cred['DRIVER']};"
+        f"SERVER={cred['SERVER']};"
+        f"DATABASE={cred['DATABASE']};"
+        f"UID={cred['UID']};"
+        f"PWD={cred['PWD']}")
+
+    engine = sqlalchemy.create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
+    df.to_sql(myblob.name, con=engine, schema='clean', if_exists='append')
     
     parquet_file = io.BytesIO()
     df.to_parquet(parquet_file, engine='pyarrow')
